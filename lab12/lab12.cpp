9b89b56467dd6107b00d583918d0a9a2ba5eabeb
Alex <aunderwood003@student.butte.edu>
2017-08-30 01:49:08 +0000

    lab 1.2 commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,19 @@
+// created by Derp made in 8-29-17
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+ cout << "        T         " << endl;
+ cout << "       TTT        " << endl;
+ cout << "      TTTTT       " << endl;
+ cout << "     TTTTTTT      " << endl;
+ cout << "    TTTTTTTTT     " << endl;
+ cout << "   T         T    " << endl;
+ cout << "  TTT       TTT   " << endl;
+ cout << " TTTTT     TTTTT  " << endl;
+ cout << "TTTTTTT   TTTTTTT " << endl;
+ cout << "TTTTTTTTTTTTTTTTTT" << endl;
+ 
+}
