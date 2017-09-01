59f243f0f760dec44ad925e1f4ad8f8735d79f2e
Alex <aunderwood003@student.butte.edu>
2017-09-01 02:03:04 +0000

    Lab 1.3 submission

---
+++ lab13/lab13.cpp
@@ -0,0 +1,14 @@
+// Made by Alex U. on 8-31-17
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numPopulation = 325779000; //current population
+    int numSecondsInYear = 31536000;
+    int numYears = 8;
+    cout << "The population of the United Staes in 2025 will be ";
+    cout << numPopulation + (numSecondsInYear / 12) * numYears;
+    cout << endl;
+    
+    return 0;
+}
